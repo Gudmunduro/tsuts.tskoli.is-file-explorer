36e0eabc79bea28b28eb1ed39390eac4f4d07be6
Guðmundur Óli <goh140400@gmail.com>
2017-03-21 23:27:30 +0000

    Update getContent.php

--- getContent.php
+++ getContent.php
@@ -1,21 +1,18 @@
 <?php
 session_start();
 
-       function contains($str, $cont)
+function contains($str, $cont)
+{
+  return strpos($str, $cont) !== FALSE;
+}
+$location = $_POST["dir"];
+$loc2 = $location;
+if (empty($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] != "true")
+{
+       if (contains($loc2, "/."))
        {
-         return strpos($str, $cont) !== FALSE;
+          $loc2 = str_replace("/.", "", $loc2);
        }
-       $location = $_POST["dir"];
-       $loc2 = $location;
-       if (empty($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] != "true")
-       {
-         if (contains($loc2, "/."))
-        {
-         $loc2 = str_replace("/.", "", $loc2);
-        }
-
-
-
        if (contains($loc2, "/////"))
        {
          $loc2 = str_replace("/////", "/", $loc2);
@@ -36,8 +33,8 @@ session_start();
        {
          die("Access Denied");
        }
-       }
+}
 
 
-        die(file_get_contents( $location ));
-    ?>
+die(file_get_contents( $location ));
+?>
